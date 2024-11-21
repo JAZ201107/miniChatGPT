464454b2da2f126ce3556cdd9c56dc8eec804eb9
NobleCatt <64055643+JAZ201107@users.noreply.github.com>
2024-11-21 15:17:59 +0800

    Change ugly number

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import time
 
 from model import GPT
 from config import GPTConfig
@@ -9,8 +10,11 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 if __name__ == "__main__":
+
+    config = GPTConfig(vocab_size=50304)
     device = get_device()
-    model = GPT(GPTConfig).to(device)
+    model = GPT(config).to(device)
+    model = torch.compile(model)
 
     train_loader = DataLoaderLite("./data/input.txt", B=4, T=32)
     count_parameter(model)
@@ -20,14 +24,17 @@ if __name__ == "__main__":
     # Train
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
     for i in range(50):
+        t0 = time.time()
         x, y = train_loader.next_batch()
         x, y = x.to(device), y.to(device)
-
         optimizer.zero_grad()
-        logits, loss = model(x, y)
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
         loss.backward()
         optimizer.step()
-
-        print_info(writer, step=i, loss=loss.item())
+        torch.cuda.synchronize()
+        t1 = time.time()
+        dt = (t1 - t0) * 1000
+        print_info(writer, step=i, loss=loss.item(), dt=dt)
 
     writer.close()
