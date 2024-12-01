920288d01010883fcf9a5ebf819353e2c975191e
NobleCatt <64055643+JAZ201107@users.noreply.github.com>
2024-12-01 21:47:50 +0800

    Change DataLoader

--- dataloader.py
+++ dataloader.py
@@ -4,43 +4,6 @@ import numpy as np
 import os
 
 
-class DataLoaderLite:
-    def __init__(
-        self,
-        B,
-        T,
-        process_rank,
-        num_processes,
-    ):
-        self.B = B
-        self.T = T
-        self.process_rank = process_rank
-        self.num_processes = num_processes
-
-        with open("./data/input.txt", "r") as f:
-            text = f.read()
-
-        enc = tiktoken.get_encoding("gpt2")
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
-        print(f"loaded {len(self.tokens)} tokens")
-        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
-
-        self.current_position = self.B * self.T * self.process_rank
-
-    def next_batch(self):
-        B, T = self.B, self.T
-        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
-        x = (buf[:-1]).view(B, T)
-        y = (buf[1:]).view(B, T)
-
-        self.current_position += B * T * self.process_rank
-
-        if self.current_position + (B * T * self.process_rank + 1) > len(self.tokens):
-            self.current_position = self.B * self.T * self.process_rank
-        return x, y
-
-
 def load_tokens(filename):
     npt = np.load(filename)
     npt = npt.astype(np.int32)  # added after video
@@ -57,7 +20,7 @@ class DataLoaderLite:
         assert split in {"train", "val"}
 
         # get the shard filenames
-        data_root = "edu_fineweb10B"
+        data_root = "/root/autodl-tmp/edu_fineweb10B"
         shards = os.listdir(data_root)
         shards = [s for s in shards if split in s]
         shards = sorted(shards)
